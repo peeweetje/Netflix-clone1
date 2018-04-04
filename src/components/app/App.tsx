c24041fd9b8a129b915ea94d3ec9f5807e6dfb57
Petra Vos | Kodision <pvos@kodision.nl>
2018-04-04 21:11:48 +0200

    add googlefontd Roboto

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,16 +1,14 @@
 import * as React from 'react';
-import '../app/App.scss';
+import '../app/App.css';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">PetFlix</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        
       </div>
     );
   }
