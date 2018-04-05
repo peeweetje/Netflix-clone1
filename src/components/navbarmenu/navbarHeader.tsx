ae2653cc93fe35819f1ead57d14d5ae0ee2b899a
Petra Vos <petravos70@gmail.com>
2018-04-05 20:15:04 +0200

    feature: added new folder

---
+++ src/components/navbarmenu/navbarHeader.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import '../navbarmenu/navbarHeader.css';
+
+class NavbarHeader extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">PetFlix</h1>
+        </header>
+
+      </div>
+    );
+  }
+}
+
+export default NavbarHeader;
