ce9dff6dc440cb3e12631c80554671c38440354e
Petra Vos <petravos70@gmail.com>
2018-04-18 20:33:33 +0200

    feat: added a new folder

---
+++ src/components/home/homePage.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import NavbarHeader from '../navbarmenu/navbarHeader';
+
+class HomePage extends React.Component<{}, {}> {
+    render() {
+      return (
+        <div>
+          <NavbarHeader />
+        </div>
+      );
+    }
+  }
+
+export default HomePage;
