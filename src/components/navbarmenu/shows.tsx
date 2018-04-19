f047964c7f68c3fdf87948f6fb966fbe3463c45d
Petra Vos <petravos70@gmail.com>
2018-04-19 20:19:18 +0200

    feat: added new component

---
+++ src/components/navbarmenu/shows.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import NavbarHeader from '../navbarmenu/navbarHeader';
+
+class Shows extends React.Component<{}, {}> {
+    render() {
+      return (
+        <div>
+          <NavbarHeader />
+          <h1>Series</h1>
+        </div>
+      );
+    }
+  }
+
+export default Shows;
