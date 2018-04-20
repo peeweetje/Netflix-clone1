4ef97509a6b843dd6d47b05c10c83f5d4e4f5b1c
Petra Vos <petravos70@gmail.com>
2018-04-20 19:53:52 +0200

    refactor: change the route of homepage

--- src/components/navbarmenu/navbarHeader.tsx
+++ src/components/navbarmenu/navbarHeader.tsx
@@ -7,9 +7,9 @@ class NavbarHeader extends React.Component {
     return (
       <div className="App">
       <nav className="navbar-menu">
-      <Link to="/HomePage"><h1>BingeWatch</h1></Link>
+      <Link to="/"><h1>BingeWatch</h1></Link>
             <ul className="nav-list">
-             <Link to="/HomePage"><li>Homepagina</li></Link>
+             <Link to="/"><li>Homepagina</li></Link>
              <Link to="/shows"><li>Series</li></Link>
              <Link to="/Movies"><li>Films</li></Link>
              <Link to="/RecentlyAdded"><li>Onlangs toegevoegd</li></Link>
