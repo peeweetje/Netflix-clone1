31d659ffaddaabcf865438705facd76a2385dc7f
Petra Vos <petravos70@gmail.com>
2018-04-23 19:54:46 +0200

    refactor: add an example api to fetch data

--- src/components/home/homePage.tsx
+++ src/components/home/homePage.tsx
@@ -1,19 +1,38 @@
 import * as React from 'react';
 import NavbarHeader from '../navbarmenu/navbarHeader';
 
-// interface Iprops {}
-// interface Istate {}
+interface IhomeProps {}
+interface IhomeState {
+  hits: Array<any>;
+}
 
-class HomePage extends React.Component<{}, {}> {
+const API = 'https://hn.algolia.com/api/v1/search?query=';
+
+class HomePage extends React.Component<IhomeProps, IhomeState> {
+
   constructor (props: string) {
     super(props);
+    this.state = {
+      hits: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch(API)
+      .then(response => response.json())
+      .then(data => this.setState({ hits: data.hits }));
   }
 
     render() {
+      const { hits } = this.state;
       return (
         <div>
           <NavbarHeader />
-          <h1>Homepage</h1>
+          {hits.map(hit =>
+          <div key={hit.objectID}>
+            <a href={hit.url}>{hit.title}</a>
+          </div>
+        )}
         </div>
       );
     }
