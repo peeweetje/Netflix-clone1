07c4c4aef69816c4210788581396880cbf54767a
Petra Vos <petravos70@gmail.com>
2018-05-29 21:15:25 +0200

    refactor: add logic to searchbar

--- src/components/navbarmenu/searchBar.tsx
+++ src/components/navbarmenu/searchBar.tsx
@@ -5,13 +5,15 @@ interface IsearchProps {}
 
 interface IsearchState {
   searchResults: Array<string>;
+  searchUrl: string;
 }
 
 class SearchBar extends React.Component< IsearchProps, IsearchState> {
   constructor(props: IsearchProps) {
     super(props);
     this.state = {
-      searchResults: []
+      searchResults: [],
+      searchUrl: ""
     };
   }
 
@@ -21,13 +23,21 @@ class SearchBar extends React.Component< IsearchProps, IsearchState> {
         this.setState({searchResults: []});
         return;
   }
-    console.log(e.target.value);
+}
+
+handleKeyUp = (query: any) => {
+  if (query === "Enter" && this.state.searchResults !== [""]) {
+    let searchUrl = `https://api.themoviedb.org/3/search/multi?query=${this.state.searchResults}&api_key=cf24f6039676d503f5763537eddf2fd3 `;
+    this.setState({
+      searchUrl
+    });
+  }
 }
 
   render() {
     return (
       <form id="Search" className="Search">
-        <input onChange={this.handleChange} type="search" placeholder="Search for a title..." />
+        <input onChange={this.handleChange} onKeyUp={this.handleKeyUp} type="search" placeholder="Search for a title..." />
       </form>
     );
   }
