ba915ddbaf0c224ddda43a5cfb289e1a98c6a359
Petra Vos <petravos70@gmail.com>
2018-09-27 20:11:43 +0200

    refactor: switch code to another file

--- src/components/navbarmenu/navbarHeader.tsx
+++ src/components/navbarmenu/navbarHeader.tsx
@@ -3,9 +3,46 @@ import { Link } from "react-router-dom";
 import SearchBar from "./searchBar";
 import "../app/App.css";
 
-class NavbarHeader extends React.Component {
+export interface IsearchState {
+  searchResults: any;
+  searchUrl: string;
+  searchQuery: string;
+}
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+class NavbarHeader extends React.Component<{}, IsearchState> {
   constructor(props: any) {
     super(props);
+    this.state = {
+      searchResults: "",
+      searchUrl: "",
+      searchQuery: ""
+    };
+  }
+
+  handleSearchChange = (e: any) => {
+    const searchQuery = e.target.value;
+    this.setState({
+      searchQuery
+    });
+
+    if (!searchQuery) {
+      this.setState({ searchQuery: "" });
+      return;
+    }
+  }
+
+  handleKeyUp = () => {
+    let searchUrl = `https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&query=${
+      this.state.searchQuery
+    }`;
+    this.setState({
+      searchUrl
+    });
+    fetch(searchUrl)
+      .then(response => response.json())
+      .then(searchResults => this.setState({ searchResults }));
+    console.log(searchUrl);
   }
 
   render() {
@@ -32,7 +69,7 @@ class NavbarHeader extends React.Component {
               <li>Mijn lijst</li>
             </Link>
           </ul>
-          <SearchBar />
+          <SearchBar onChange={this.handleSearchChange} onKeyUp={this.handleKeyUp} />
         </nav>
       </div>
     );
--- src/components/navbarmenu/searchBar.tsx
+++ src/components/navbarmenu/searchBar.tsx
@@ -1,27 +1,27 @@
 import * as React from "react";
 import "../app/App.css";
 
-interface IsearchProps {}
+export interface IsearchProps {
+  handleSearchChange: any;
+  searchQuery: string;
+  handleKeyUp: any;
+}
 
-interface IsearchState {
+/* interface IsearchState {
   searchResults: any;
   searchUrl: string;
   searchQuery: string;
-}
+ }*/
 
-const API_KEY = process.env.REACT_APP_API_KEY;
+// const API_KEY = process.env.REACT_APP_API_KEY;
 
-class SearchBar extends React.Component<IsearchProps, IsearchState> {
+class SearchBar extends React.Component<IsearchProps, {}> {
   constructor(props: IsearchProps) {
     super(props);
-    this.state = {
-      searchResults: "",
-      searchUrl: "",
-      searchQuery: ""
-    };
+
   }
 
-  handleSearchChange = (e: any) => {
+  /*handleSearchChange = (e: any) => {
     const searchQuery = e.target.value;
     this.setState({
       searchQuery
@@ -34,24 +34,25 @@ class SearchBar extends React.Component<IsearchProps, IsearchState> {
   }
 
   handleKeyUp = () => {
-      let searchUrl = `https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&query=${
-        this.state.searchQuery
-      }`;
-      this.setState({
-        searchUrl
-      });
-      fetch(searchUrl)
+    let searchUrl = `https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&query=${
+      this.state.searchQuery
+    }`;
+    this.setState({
+      searchUrl
+    });
+    fetch(searchUrl)
       .then(response => response.json())
-      .then(searchResults => this.setState({ searchResults}));
-  }
+      .then(searchResults => this.setState({ searchResults }));
+    console.log(searchUrl);
+  }*/
 
   render() {
     return (
       <form id="Search" className="Search">
         <input
-          onChange={this.handleSearchChange}
-          value={this.state.searchQuery}
-          onKeyUp={this.handleKeyUp}
+          onChange={this.props.handleSearchChange}
+          value={this.props.searchQuery}
+          onKeyUp={this.props.handleKeyUp}
           type="search"
           placeholder="Search for a title..."
         />
