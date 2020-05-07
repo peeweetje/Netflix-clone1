1753fdfd8e984108c7a1b98cd8474a8920dbb630
peeweetje <petravos70@gmail.com>
2020-05-07 21:42:59 +0200

    Refactor:remove code.

--- src/components/navbarmenu/searchBar.tsx
+++ src/components/navbarmenu/searchBar.tsx
@@ -2,23 +2,27 @@ import * as React from "react";
 import "../app/App.css";
 
 export interface IsearchProps {
-  onChange?: any;
+  onChange: any;
   searchQuery?: string;
   onKeyUp?: any;
 }
 
-const SearchBar: React.FC<IsearchProps> = ({searchQuery, onKeyUp, onChange}) => {
-    return (
-      <form id="Search" className="Search">
-        <input
-          onChange={onChange}
-          value={searchQuery}
-          onKeyUp={onKeyUp}
-          type="search"
-          placeholder="Search for a title..."
-        />
-      </form>
-    );
-  };
+const SearchBar: React.FC<IsearchProps> = ({
+  searchQuery,
+  onKeyUp,
+  onChange,
+}) => {
+  return (
+    <form id="Search" className="Search">
+      <input
+        onChange={onChange}
+        value={searchQuery}
+        onKeyUp={onKeyUp}
+        type="search"
+        placeholder="Search for a title..."
+      />
+    </form>
+  );
+};
 
 export default SearchBar;
