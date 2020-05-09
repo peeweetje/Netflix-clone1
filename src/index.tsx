5d742372f20098c696f2a068d3f465cdd8ad2394
peeweetje <petravos70@gmail.com>
2020-05-09 15:08:57 +0200

    Feature: add a themeProvider.

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 import App from "./components/app/App";
 import registerServiceWorker from "./registerServiceWorker";
-import "./index.css";
+import theme from "../src/styles/theme";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>,
   document.getElementById("root") as HTMLElement
 );
 registerServiceWorker();
