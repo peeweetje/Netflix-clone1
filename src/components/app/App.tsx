e1ae9a20d3e097c4fc2011d373008a622163f085
peeweetje <petravos70@gmail.com>
2020-05-10 20:24:12 +0200

    Add global styling

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,24 +1,26 @@
 import * as React from "react";
 import { Route, Switch } from "react-router-dom";
 import HomePage from "../home/homePage";
-import Shows from "../navbarmenu/shows";
-import Movies from "../navbarmenu/movies";
-import RecentlyAdded from "../navbarmenu/recentlyAdded";
-import MyList from "../navbarmenu/myList";
+import Shows from "../navbarmenu/shows/shows";
+import Movies from "../navbarmenu/movies/movies";
+import RecentlyAdded from "../navbarmenu/recently-added/recentlyAdded";
+import MyList from "../navbarmenu/my-list/myList";
+import { GlobalStyle } from "../../../src/styles/global";
 import "../app/App.css";
 
-const App = () =>  {
-    return (
-      <div>
-        <Switch>
-          <Route exact={true} path="/" component={HomePage} />
-          <Route exact={true} path="/Shows" component={Shows} />
-          <Route exact={true} path="/Movies" component={Movies} />
-          <Route exact={true} path="/RecentlyAdded" component={RecentlyAdded} />
-          <Route exact={true} path="/MyList" component={MyList} />
-        </Switch>
-      </div>
-    );
+const App = () => {
+  return (
+    <div>
+      <GlobalStyle />
+      <Switch>
+        <Route exact={true} path="/" component={HomePage} />
+        <Route exact={true} path="/Shows" component={Shows} />
+        <Route exact={true} path="/Movies" component={Movies} />
+        <Route exact={true} path="/RecentlyAdded" component={RecentlyAdded} />
+        <Route exact={true} path="/MyList" component={MyList} />
+      </Switch>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/styles/global.tsx
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Roboto', sans-serif;
+    background-color: #333;
+    font-family: 'Roboto'
+}
+a {
+    text-decoration: none;
+    color:  #8eb3cc;
+}
+ul {
+    list-style: none;
+  }
+* {
+    box-sizing: border-box;
+}
+`;
