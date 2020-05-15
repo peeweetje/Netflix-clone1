8de63ad335cdddcc253bd572361e08b038cc7f2c
peeweetje <petravos70@gmail.com>
2020-05-15 14:48:24 +0200

    Refactor: change the styling of the navbar making more use of flexbox.

--- src/components/navbarmenu/navbar-styles.tsx
+++ src/components/navbarmenu/navbar-styles.tsx
@@ -4,41 +4,37 @@ import styled from "styled-components";
            NavbarHeader
 ==================================*/
 export const NavbarContainer = styled.div`
-  h1 {
-    color: #8eb3cc;
-    padding-left: 15px;
-  }
-
-  li {
-    padding-left: 37px;
-  }
+  display: flex;
+  padding: 6px;
 `;
 
 export const NavbarMenu = styled.nav`
   display: flex;
-  justify-content: flex-start;
+  justify-content: space-between;
 `;
 
 export const NavList = styled.ul`
-  display: flex;
-  justify-content: center;
-  padding: 11px;
+display: flex;
+li{
+  padding 18px;
+  white-space: nowrap;
+}
+li: first-of-type {
+  font-size: 30px;
+  padding: 8px;
+} 
 `;
 
 /*==================================
           SearchBar
 ==================================*/
 export const NavbarSearch = styled.form`
-  margin-top: 24px;
-  margin-left: 5%;
-  border-radius: 1px solid #000;
+  margin-top: 26px;
+  margin-left: 30%;
 `;
 
 export const NavbarInput = styled.input`
   border: 1px solid black;
   border-radius: 5px;
-  padding: 5px;
-  width: 134%;
-  border-radius: 1px solid #000;
-  outline: none;
+  padding: 8px;
 `;
