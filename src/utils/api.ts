5d537b968952d9bd9a2990b8c17916ed0d099626
PEEWEE\petra <petravos70@gmail.com>
2022-07-16 10:31:40 +0200

    Create variable for search api.

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,3 +1,4 @@
  export const API_KEY = process.env.REACT_APP_API_KEY;
  export let discoverMovieUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
  export let imageUrl = `https://image.tmdb.org/t/p/w300`;
+ export let searchMovieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query`;
