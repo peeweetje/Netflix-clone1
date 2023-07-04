3011efdd77ab6bfbc307375f5d0782d9527bcace
PEEWEE\petra <petravos70@gmail.com>
2023-07-04 20:22:33 +0200

    Set styled-system in ignore file. set up panda and make sure it works.

--- netflix-create-next-app/src/app/page.tsx
+++ netflix-create-next-app/src/app/page.tsx
@@ -1,7 +1,17 @@
+import { css } from '../../styled-system/css';
+
 export default function Home() {
   return (
     <main>
-      <div>hello page </div>
+      <div
+        className={css({
+          fontSize: '2xl',
+          fontWeight: 'bold',
+          color: 'amber.500',
+        })}
+      >
+        Hello 🐼!
+      </div>
     </main>
   );
 }
