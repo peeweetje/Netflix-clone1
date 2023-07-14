b142f46251de2237e7bd9bb47a2450844ee294f1
PEEWEE\petra <petravos70@gmail.com>
2023-07-14 20:36:36 +0200

    Set the google font Poppins in the layout file.

--- netflix-create-next-app/src/app/page.tsx
+++ netflix-create-next-app/src/app/page.tsx
@@ -1,14 +1,8 @@
 import { css } from '../../styled-system/css';
-import { Poppins } from 'next/font/google';
-
-const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['400'],
-});
 
 export default function Home() {
   return (
-    <main className={`${poppins.className}`}>
+    <main>
       <h1
         className={css({
           color: 'amber.500',
