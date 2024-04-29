a79990dede71d60b41d668afe3cbd17c63669e24
INFINITAS\petra.vos <pvos@infinitaslearning.com>
2024-04-29 19:43:24 +0200

    Make the navbar responsive.

--- netflix-create-react-app/src/components/navbarmenu/navbarheader/navbar-styles.tsx
+++ netflix-create-react-app/src/components/navbarmenu/navbarheader/navbar-styles.tsx
@@ -1,9 +1,13 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const NavbarMenu = styled.nav`
   display: flex;
   width: 100%;
   justify-content: center;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
 `;
 
 export const NavList = styled.ul`
@@ -18,13 +22,23 @@ export const NavList = styled.ul`
   li:hover {
     text-decoration: 5px underline;
   }
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+  }
 `;
 
 export const BrandContainer = styled.div`
   display: flex;
   align-items: center;
   margin-right: auto;
-  font-size: ${(props) => props.theme.fontSize[3]};
+  font-size: ${(props) => props.theme.fontSize[2]};
   padding-left: ${(props) => props.theme.space[8]};
   list-style: none;
+
+  @media (max-width: 768px) {
+    justify-content: center;
+    margin-right: 0;
+  }
 `;
