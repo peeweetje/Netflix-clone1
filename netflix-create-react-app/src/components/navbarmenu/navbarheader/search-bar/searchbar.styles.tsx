a0ca7eef5acb866b7b63c86cf8faa57524aeed4a
INFINITAS\petra.vos <pvos@infinitaslearning.com>
2024-04-29 19:43:56 +0200

    Make the searchbar responsive.

--- netflix-create-react-app/src/components/navbarmenu/navbarheader/search-bar/searchbar.styles.tsx
+++ netflix-create-react-app/src/components/navbarmenu/navbarheader/search-bar/searchbar.styles.tsx
@@ -1,8 +1,15 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const NavbarSearch = styled.div`
   padding: ${(props) => props.theme.space[8]};
   margin-left: auto;
+
+  @media (max-width: 768px) {
+    margin-left: 0;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+  }
 `;
 
 export const NavbarInput = styled.input`
@@ -10,4 +17,9 @@ export const NavbarInput = styled.input`
   border-radius: ${(props) => props.theme.borderRadius[2]};
   padding: ${(props) => props.theme.space[4]};
   margin: ${(props) => props.theme.space[4]};
+  cursor: pointer;
+
+  @media (max-width: 768px) {
+    width: 75%;
+  }
 `;
