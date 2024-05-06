6fc5d81415cde6837477e1ffdb26e7932104ddb3
PEEWEE\petra <petravos70@gmail.com>
2024-05-06 20:19:05 +0200

    Update theme.

--- netflix-create-react-app/src/i18n.ts
+++ netflix-create-react-app/src/i18n.ts
@@ -3,26 +3,34 @@ import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import HttpApi from 'i18next-http-backend';
 
-
 i18n
   .use(initReactI18next)
   .use(LanguageDetector)
   .use(HttpApi)
   .init({
     supportedLngs: ['en', 'nl'],
-     lng: 'en',
+    lng: 'en',
     fallbackLng: 'en',
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
     detection: {
-      order: ['querystring', 'cookie', 'localStorage', 'sessionStorage', 'navigator', 'htmlTag', 'path', 'subdomain'],
+      order: [
+        'querystring',
+        'cookie',
+        'localStorage',
+        'sessionStorage',
+        'navigator',
+        'htmlTag',
+        'path',
+        'subdomain',
+      ],
       caches: ['cookie'],
     },
-    backend: { 
+    backend: {
       loadPath: '/locales/{{lng}}/translation.json',
     },
-    react: {useSuspense: false}
+    react: { useSuspense: false },
   });
 
 export default i18n;
