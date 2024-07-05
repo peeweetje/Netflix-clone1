f88ed78c7cf12d37ab8fc8828edbd1266e95f303
PEEWEE\petra <petravos70@gmail.com>
2024-07-05 11:42:25 +0200

    Remove create-react-app.

--- netflix-create-react-app/src/components/home/homepage-styles.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const MainContainer = styled.main`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  flex-wrap: wrap;
-  margin-top: ${(props) => props.theme.space[11]};
-`;
--- netflix-create-react-app/src/components/spinner/spinner.tsx
+++
@@ -1,8 +0,0 @@
-import { SpinnerWrapper } from './spinner.styles';
-import React from 'react';
-
-const Spinner = () => {
-  return <SpinnerWrapper />;
-};
-
-export default Spinner;
