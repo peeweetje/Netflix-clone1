420fdaad6c753213b90fbed87058c9ca2787a7de
PEEWEE\petra <petravos70@gmail.com>
2024-11-09 19:38:25 +0100

    Add config so server opens.

--- netflix-create-react-vite-app/vite.config.ts
+++ netflix-create-react-vite-app/vite.config.ts
@@ -6,6 +6,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: true,
+  },
   test: {
     globals: true,
     environment: 'jsdom',
