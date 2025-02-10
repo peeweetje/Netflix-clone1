715967400b9151b5b3cbd2041979ae7aedcd731d
PEEWEE\petra <petravos70@gmail.com>
2025-02-10 19:19:36 +0100

    Update home page using the hook and the new component.

--- netflix-create-react-vite-app/src/components/home/homePage.tsx
+++ netflix-create-react-vite-app/src/components/home/homePage.tsx
@@ -1,18 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
 import { MainContainer } from './homepage-styles';
 import Card from '../card/card';
 import NavbarHeader from '../navbarmenu/navbarheader/navbarHeader';
 import { discoverMovieUrl, imageUrl } from '../../utils/api';
 import Spinner from '../spinner/spinner';
-
-type MovieResult = {
-  id: number;
-  poster_path: string;
-  overview: string;
-  title: string;
-  vote_average: number;
-};
+import { useFetchMovies } from '../hooks/useFetchMovies';
+import { MovieList } from '../movie-list/movieList';
+import { MovieResult } from '../../utils/types/types';
 
 const Homepage = () => {
   const [results, setResults] = useState<MovieResult[]>([]);
@@ -24,92 +19,44 @@ const Homepage = () => {
 
   const VITE_API_KEY = import.meta.env.VITE_API_KEY;
 
-  let searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${VITE_API_KEY}&query=${searchQuery}`;
+  const searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${VITE_API_KEY}&query=${searchQuery}`;
 
-  useEffect(() => {
-    const fetchMovies = async () => {
-      try {
-        const response = await fetch(searchUrl);
-        const data = await response.json();
-        setSearchMovies(data.results);
-      } catch (error) {
-        setError('Failed to fetch movies. Please try again later.');
-      } finally {
-        setLoading(false);
+  useFetchMovies(searchUrl, setSearchMovies, setLoading, setError);
+  useFetchMovies(discoverMovieUrl, setResults, setLoading, setError);
+
+  const handleSearch = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>): void => {
+      e.preventDefault();
+      const newSearchQuery = e.target.value;
+      setSearchQuery(newSearchQuery);
+
+      if (!newSearchQuery) {
+        setSearchQuery('');
+        setSearchMovies([]);
       }
-    };
-
-    fetchMovies();
-  }, [searchUrl, setSearchMovies]);
-
-  useEffect(() => {
-    const fetchDiscoverMovies = async () => {
-      try {
-        const response = await fetch(discoverMovieUrl);
-        const { results } = await response.json();
-        setResults(results);
-      } catch (error) {
-        setError('Failed to fetch movies. Please try again later.');
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchDiscoverMovies();
-  }, [discoverMovieUrl, setResults]);
-
-  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    e.preventDefault();
-    const newSearchQuery = e.target.value;
-    setSearchQuery(newSearchQuery);
-
-    if (!newSearchQuery) {
-      setSearchQuery('');
-      setSearchMovies([]);
-    }
-  };
+    },
+    []
+  );
 
   return (
     <>
-      <NavbarHeader value={searchQuery} onChange={handleSearchChange} />
+      <NavbarHeader value={searchQuery} onChange={handleSearch} />
       <MainContainer aria-label={t('movie-listings')}>
-        {loading ? (
+        {loading && (
           <div>
             <Spinner />
             <p>{t('loading')}</p>
           </div>
-        ) : error ? (
+        )}
+        {error && (
           <div>
             <p>Error: {error}</p>
           </div>
-        ) : searchMovies.length > 0 ? (
-          searchMovies.map(
-            (result: MovieResult) =>
-              result.poster_path && (
-                <Card
-                  key={result.id}
-                  src={`${imageUrl}${result.poster_path}`}
-                  alt={t('movie-poster')}
-                  overview={result.overview}
-                  title={result.title}
-                  vote_average={result.vote_average}
-                />
-              )
-          )
-        ) : (
-          results.map(
-            (result: MovieResult) =>
-              result.poster_path && (
-                <Card
-                  key={result.id}
-                  src={`${imageUrl}${result.poster_path}`}
-                  alt={t('movie-poster')}
-                  overview={result.overview}
-                  title={result.title}
-                  vote_average={result.vote_average}
-                />
-              )
-          )
+        )}
+        {!loading && !error && (
+          <MovieList
+            movies={searchMovies.length > 0 ? searchMovies : results}
+          />
         )}
       </MainContainer>
     </>
