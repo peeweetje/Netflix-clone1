389f9c95abc987f4927a46e34156f8422d5fa5b9
PEEWEE\petra <petravos70@gmail.com>
2025-07-10 09:32:03 +0200

    Add navbarHeader and search funtionality.

--- netflix-create-react-vite-app/src/pages/shows/shows.tsx
+++ netflix-create-react-vite-app/src/pages/shows/shows.tsx
@@ -4,8 +4,10 @@ import { Card } from '../../components/card/card';
 import { Spinner } from '../../components/spinner/spinner';
 import { imageUrl, trendingShowUrl } from '../../utils/api';
 import type { ShowResult } from '../../utils/types/types';
+import { NavbarHeader } from '../../components/navbarmenu/navbarheader/navbarHeader';
+import { useTranslation } from 'react-i18next';
 
- const StyledContainer = styled.div`
+const StyledContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
   gap: 1rem;
@@ -13,8 +15,14 @@ import type { ShowResult } from '../../utils/types/types';
 
 export const Shows = () => {
   const [shows, setShows] = useState<ShowResult[]>([]);
+  const [searchShows, setSearchShows] = useState<ShowResult[]>([]);
+  const [searchQuery, setSearchQuery] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
+  const { t } = useTranslation();
+
+  const VITE_API_KEY = import.meta.env.VITE_API_KEY;
+  const searchUrl = `https://api.themoviedb.org/3/search/tv?api_key=${VITE_API_KEY}&query=${searchQuery}`;
 
   React.useEffect(() => {
     const fetchShows = async () => {
@@ -34,11 +42,44 @@ export const Shows = () => {
     fetchShows();
   }, []);
 
+  React.useEffect(() => {
+    if (!searchQuery) {
+      setSearchShows([]);
+      return;
+    }
+    const fetchSearchShows = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(searchUrl);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const { results } = await response.json();
+        setSearchShows(results);
+      } catch (err) {
+        setError('Failed to fetch shows. Please try again later.');
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchSearchShows();
+  }, [searchQuery, searchUrl]);
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    e.preventDefault();
+    const newSearchQuery = e.target.value;
+    setSearchQuery(newSearchQuery);
+    if (!newSearchQuery) {
+      setSearchShows([]);
+    }
+  };
+
   if (loading) {
     return (
       <div>
+        <NavbarHeader onChange={handleSearch} value={searchQuery} />
         <Spinner />
-        <p>Loading...</p>
+        <p>{t ? t('loading') : 'Loading...'}</p>
       </div>
     );
   }
@@ -46,6 +87,7 @@ export const Shows = () => {
   if (error) {
     return (
       <div>
+        <NavbarHeader onChange={handleSearch} value={searchQuery} />
         <p>{error}</p>
       </div>
     );
@@ -53,9 +95,10 @@ export const Shows = () => {
 
   return (
     <div>
+      <NavbarHeader onChange={handleSearch} value={searchQuery} />
       <h1>Best Shows</h1>
       <StyledContainer>
-        {shows.map(
+        {(searchShows.length > 0 ? searchShows : shows).map(
           (show) =>
             show.poster_path && (
               <Card
