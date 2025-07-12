2f6c3a8d09ba7469b07ac83911c6df81973bad8c
PEEWEE\petra <petravos70@gmail.com>
2025-07-12 10:45:55 +0200

    Add typings for the cast members.

--- netflix-create-react-vite-app/src/utils/types/types.ts
+++ netflix-create-react-vite-app/src/utils/types/types.ts
@@ -13,3 +13,17 @@ export type ShowResult = {
   name: string;
   vote_average: number;
 };
+
+export interface Actor {
+  cast_id?: string | number;
+  credit_id?: string | number;
+  profile_path?: string | null;
+  name: string;
+  character: string;
+}
+
+export interface CastMemberProps {
+  actor: Actor;
+  src: string;
+  alt: string;
+}
