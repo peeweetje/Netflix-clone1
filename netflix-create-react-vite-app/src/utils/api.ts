165f990579fd5a8c5c880775f7841584ba707765
PEEWEE\petra <petravos70@gmail.com>
2025-07-14 21:34:45 +0200

    Add new movieUrls for the homepage.

--- netflix-create-react-vite-app/src/utils/api.ts
+++ netflix-create-react-vite-app/src/utils/api.ts
@@ -4,3 +4,6 @@ export const imageUrl = 'https://image.tmdb.org/t/p/original/';
 export const searchMovieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${VITE_API_KEY}&query`;
 export const discoverShowUrl = `https://api.themoviedb.org/3/discover/tv?api_key=${VITE_API_KEY}`;
 export const trendingShowUrl = `https://api.themoviedb.org/3/trending/tv/week?api_key=${VITE_API_KEY}`;
+export const popularMoviesUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${VITE_API_KEY}`;
+export const topRatedMoviesUrl = `https://api.themoviedb.org/3/movie/top_rated?api_key=${VITE_API_KEY}`;
+export const actionMoviesUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${VITE_API_KEY}&with_genres=28`;
