1e3da95fda5e38a923a93cd76c1de70eb9147b38
PEEWEE\petra <petravos70@gmail.com>
2025-07-16 12:29:08 +0200

    Add the remove button to the movie list.

--- netflix-create-react-vite-app/src/components/movie-list/movieList.tsx
+++ netflix-create-react-vite-app/src/components/movie-list/movieList.tsx
@@ -1,28 +1,48 @@
 import { useTranslation } from 'react-i18next';
-import { FlexWrapper, StyledLink } from './movie.styles';
+import {
+  FlexWrapper,
+  CardWrapper,
+  RemoveButton,
+  StyledLink,
+} from './movie.styles';
 import { imageUrl } from '../../utils/api';
 import type { MovieResult } from '../../utils/types/types';
 import { Card } from '../Card/Card';
+import styled from 'styled-components';
 
-
-
-export const MovieList = ({ movies }: { movies: MovieResult[] }) => {
+export const MovieList = ({
+  movies,
+  removeFromList,
+}: {
+  movies: MovieResult[];
+  removeFromList?: (item: any) => void;
+}) => {
   const { t } = useTranslation();
 
   return (
     <FlexWrapper>
       {movies.map(
         (result: MovieResult) =>
-          result.poster_path && (
-            <StyledLink to={`/Movies/${result.id}`} key={result.id}>
-              <Card
-                alt={t('movie-poster')}
-                overview={result.overview}
-                src={`${imageUrl}${result.poster_path}`}
-                title={result.title}
-                vote_average={result.vote_average}
-              />
-            </StyledLink>
+          result.poster_path &&
+          result.id && (
+            <CardWrapper key={result.id}>
+              <StyledLink to={`/movies/${result.id}`}>
+                <Card
+                  alt={t('movie-poster')}
+                  overview={result.overview}
+                  src={`${imageUrl}${result.poster_path}`}
+                  title={result.title}
+                  vote_average={result.vote_average}
+                  id={result.id}
+                  media_type='movie'
+                />
+              </StyledLink>
+              {removeFromList && (
+                <RemoveButton onClick={() => removeFromList(result)}>
+                  Remove
+                </RemoveButton>
+              )}
+            </CardWrapper>
           )
       )}
     </FlexWrapper>
