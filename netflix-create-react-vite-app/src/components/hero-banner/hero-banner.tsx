1f9b9c7292941d9590151d3d4f1bd74948ae7b50
PEEWEE\petra <petravos70@gmail.com>
2025-07-28 12:28:25 +0200

    Make the animation more smoother for the snow and leafs.

--- netflix-create-react-vite-app/src/components/hero-banner/hero-banner.tsx
+++ netflix-create-react-vite-app/src/components/hero-banner/hero-banner.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { BannerContainer, BannerOverlay, BannerTitle, BannerOverview, BannerButtons, BannerButton, SnowContainer, Snow } from './hero-banner.styles';
+import { BannerContainer, BannerOverlay, BannerTitle, BannerOverview, BannerButtons, BannerButton, SnowContainer, Snow, LeafContainer, Leaf } from './hero-banner.styles';
 import { useTheme } from '../../context/themeContext';
-import { winterTheme } from '../../styles/themes/themes';
+import { springTheme, winterTheme } from '../../styles/themes/themes';
+import { LeafIcon } from '../svg/leafs';
 
 interface HeroBannerProps {
   backgroundImage: string;
@@ -23,14 +24,39 @@ export const HeroBanner = ({
   const renderSnow = () => (
     <SnowContainer>
       {Array.from({ length: 50 }).map((_, i) => (
-        <Snow key={i} />
+        <Snow
+          key={i}
+          style={{
+            left: `${Math.random() * 100}%`,
+            animationDuration: `${Math.random() * 10 + 5}s`, // 5 to 15 seconds
+            animationDelay: `${Math.random() * 10}s`,
+          }}
+        />
       ))}
     </SnowContainer>
+  );
+
+  const renderLeaves = () => (
+    <LeafContainer>
+      {Array.from({ length: 50 }).map((_, i) => (
+        <Leaf
+          key={i}
+          style={{
+            left: `${Math.random() * 100}%`,
+            animationDuration: `${Math.random() * 10 + 5}s`, // 5 to 15 seconds
+            animationDelay: `${Math.random() * 10}s`,
+          }}
+        >
+          <LeafIcon color={theme.colors.primary} />
+        </Leaf>
+      ))}
+    </LeafContainer>
   );
 
   return (
     <BannerContainer backgroundImage={backgroundImage}>
       {theme.name === winterTheme.name && renderSnow()}
+      {theme.name === springTheme.name && theme.colors.leafIcon && renderLeaves()}
       <BannerOverlay>
         <div>
           <BannerTitle>{title}</BannerTitle>
