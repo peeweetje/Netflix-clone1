e83e8d89709ed50d0a3e736697a15ca960996e74
PEEWEE\petra <petravos70@gmail.com>
2025-08-02 09:50:40 +0200

    Create styling. Mostly the flapping of the wings.

---
+++ netflix-create-react-vite-app/src/components/svg/bee.styles.tsx
@@ -0,0 +1,21 @@
+import styled, { keyframes } from 'styled-components';
+import { motion } from 'framer-motion';
+
+export const wingFlap = keyframes`
+  0% {
+    transform: rotateZ(0deg);
+  }
+  50% {
+    transform: rotateZ(20deg);
+  }
+  100% {
+    transform: rotateZ(0deg);
+  }
+`;
+
+export const AnimatedWings = styled.g`
+  animation: ${wingFlap} 0.1s linear infinite;
+  transform-origin: 50% 50%;
+`;
+
+export const AnimatedBee = styled(motion.div)``;
