5011641c22eb3edca182b2ff7f5f5b699e08c103
PEEWEE\petra <petravos70@gmail.com>
2025-10-01 13:02:41 +0200

    Add config for tanstack devtools.

--- netflix-create-react-vite-app/src/main.tsx
+++ netflix-create-react-vite-app/src/main.tsx
@@ -4,12 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 import '../i18n';
 import { App } from './app/App';
 import { MyListProvider } from './context/myListContext';
+import { TanStackDevtools } from '@tanstack/react-devtools';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <MyListProvider>
         <App />
+        <TanStackDevtools />
       </MyListProvider>
     </BrowserRouter>
   </React.StrictMode>
