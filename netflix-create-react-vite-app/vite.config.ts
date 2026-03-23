bd37376868fa602c4579123d4262e00c636918d1
peeweetje <petravos70@gmail.com>
2026-03-23 10:36:57 +0100

    Update config file.

--- netflix-create-react-vite-app/vite.config.ts
+++ netflix-create-react-vite-app/vite.config.ts
@@ -1,20 +1,11 @@
 /// <reference types="vite/client"/>
-/// <reference types= "vitest"/>
-
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { devtools } from '@tanstack/devtools-vite';
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [devtools(), react()],
-  server: {
-    open: true,
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    css: true,
-    setupFiles: './src/setup.ts',
-  },
+    plugins: [devtools(), react()],
+    server: {
+        open: true,
+    },
 });
